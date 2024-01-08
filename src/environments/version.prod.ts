5a48bc1f297077ff255b182d4cb53ce40c8b7e9e
Yogeshwaran Chandrakasan <yogeshwaran.c@finstein.ai>
2024-01-08 16:32:13 +0530

    test version update

---
+++ src/environments/version.prod.ts
@@ -0,0 +1,4 @@
+
+export const version = '0.1.1';
+export const buildDate = '2024-01-08T11:00:27.664Z';
+export const commitHash = '376ea53349d978f1b27794029cccc355db7b2982';
---
+++ src/environments/version.ts
@@ -0,0 +1,3 @@
+export const version = '0.0.0-development';
+export const buildDate = Date.now();
+export const commitHash = 'aaaaaa';
